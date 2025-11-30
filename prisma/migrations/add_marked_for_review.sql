3ea203df0e8cb46dbd1f4439d55aaf1e274806d0
matthewnoahkim <mattnoahkim@gmail.com>
2025-11-30 11:53:42 -0800

    clean up

--- prisma/migrations/add_marked_for_review.sql
+++
@@ -1,5 +0,0 @@
--- Add markedForReview column to AttemptAnswer table
--- Migration: add_marked_for_review
-
-ALTER TABLE "AttemptAnswer" ADD COLUMN IF NOT EXISTS "markedForReview" BOOLEAN NOT NULL DEFAULT false;
-
